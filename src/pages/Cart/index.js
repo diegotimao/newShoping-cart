7c99449e14b7b63debdc4ae86a574f3191a5b7c1
diegotimao <diegotimao1104@gmail.com>
2022-05-15 17:33:02 -0500

    Configurações iniciais do aplicativo Web

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <h1>Seu carrinho não contém nenhum item</h1>
+    )
+  }
+}
+
+export default Cart;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <h1>Bem vindo ao Shoping Cart</h1>
+    )
+  }
+}
+
+export default Home;
